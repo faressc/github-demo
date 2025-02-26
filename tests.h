0a0ad55ec2c8b379d446953170b9aea744d02e1a
Fares Schulz <schulzfares@gmail.com>
2025-02-26 13:48:01 +0100

    new checks handeling

--- tests.h
+++ tests.h
@@ -10,6 +10,7 @@ void TestAdd() {
     assert(asc.add(1) == 2);
     assert(asc.add(-1) == 1);
     assert(asc.add(-2) == -1);
+    assert(asc.add(-2) == -1);
     std::cout << "All Add tests passed!" << std::endl;
 }
 
